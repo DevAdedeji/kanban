fbdf5ec72a80832131aacf8e1e9bc5c234f4da14
DevAdedeji <adedejitewogbade2@gmail.com>
2023-10-17 20:43:53 +0100

    WIP

--- composables/useTheme.ts
+++ composables/useTheme.ts
@@ -1,16 +1,16 @@
 const useTheme = () => {
   const getTheme = () => {
-    if (process.client) {
-      const theme = document.documentElement.className;
-      return theme;
-    }
+    const theme = document.documentElement.className;
+    return theme;
   };
   const setTheme = (theme: string) => {
-    if (process.client) {
-      document.documentElement.className = theme;
-    }
+    document.documentElement.className = theme;
   };
-  return { getTheme, setTheme };
+  const toggleTheme = () => {
+    const theme = getTheme();
+    theme === "dark" ? setTheme("light") : setTheme("dark");
+  };
+  return { getTheme, setTheme, toggleTheme };
 };
 
 export default useTheme;
