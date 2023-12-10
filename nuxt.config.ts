7c465494b293d48a242e2bf57130910d9f56c647
DevAdedeji <adedejitewogbade2@gmail.com>
2023-12-10 14:09:14 +0100

    Added meta tags for seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,32 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: "en",
       },
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content: "Kanban: Manage your tasks.",
+        },
+        { name: "format-detection", content: "telephone=no" },
+        {
+          hid: "og:title",
+          property: "og:title",
+          content: "Kanban: Manage your tasks.",
+        },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content: "Kanban, your task management app.",
+        },
+        {
+          hid: "og:image",
+          property: "og:image",
+          content:
+            "https://drive.google.com/uc?id=1AN1U9YUx1PQfTjvee5HrEsmZskoEFsm5",
+        },
+      ],
     },
   },
   modules: ["@vueuse/nuxt", "@nuxt/ui", "@nuxtjs/supabase"],
